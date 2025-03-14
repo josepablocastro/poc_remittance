3ab84487b01fbbc80b736daef9d6dcb9b3076ca5
Jose Pablo Castro <jp@gobu.io>
2025-03-14 11:30:15 -0600

    Se convierten las llaves del json

--- internal/application/core/domain/payment.go
+++ internal/application/core/domain/payment.go
@@ -3,14 +3,14 @@ package domain
 import "time"
 
 type Payment struct {
-	ID          int64
-	Sender      string
-	Beneficiary string
-	Amount      float32
-	Currency    string
-	Number      string
-	State       string
-	CreatedAt   int64
+	ID          int64   `json:"id"`
+	Sender      string  `json:"sender"`
+	Beneficiary string  `json:"beneficiary"`
+	Amount      float32 `json:"amount"`
+	Currency    string  `json:"currency"`
+	Number      string  `json:"number"`
+	State       string  `json:"state"`
+	CreatedAt   int64   `json:"created_at"`
 }
 
 func ReceivePayment(number string, sender string, beneficiary string, amount float32, currency string) Payment {
